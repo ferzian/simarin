0fa2282c8c72fc61286f9ca0efdf21fa87eaef26
ferzian <m.ferzian09@gmail.com>
2025-07-31 08:58:22 +0700

    Update script rekap data peserta

--- routes/admin/peserta.js
+++ routes/admin/peserta.js
@@ -4,23 +4,40 @@ const { User, Participant } = require('../../models');
 const { isAuthenticated, isAdmin } = require('../../middleware/authMiddleware');
 
 router.get('/peserta', isAuthenticated, isAdmin, async (req, res) => {
-    try {
-        const pendingUsers = await User.findAll({
-            where: { role: 'user', approved: false },
-        });
+  try {
+    const participants = await Participant.findAll({
+      include: [
+        {
+          model: User,
+          where: { role: 'user', approved: true }, // hanya user yang approved
+        },
+      ],
+    });
 
-        const pendingParticipants = await Participant.findAll({
-            where: { statusSelesai: false },
-            include: [{ model: User }]
-        });
-        res.render('admin/peserta', {
-            pendingUsers,
-            pendingParticipants
-        });
-    } catch (err) {
-        console.error('❌ Gagal ambil data approval akun:', err);
-        res.status(500).send('Internal Server Error');
-    }
+    const pendingUsers = await User.findAll({ where: { role: 'user', approved: false } });
+    const pendingParticipants = await Participant.findAll({
+      where: { statusSelesai: false },
+      include: [{ model: User }]
+    });
+
+    res.render('admin/peserta', {
+      participants: participants.map((p) => ({
+        name: p.User.username,
+        gender: p.jenisKelamin,
+        major: p.jurusan,
+        type: p.jenisKegiatan,
+        institution: p.asalInstansi,
+        startDate: p.tanggalMulai.toISOString().split('T')[0],
+        endDate: p.tanggalSelesai.toISOString().split('T')[0],
+      })),
+      pendingUsers,
+      pendingParticipants,
+      user: req.session.user
+    });
+  } catch (err) {
+    console.error('❌ Gagal ambil data peserta:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
